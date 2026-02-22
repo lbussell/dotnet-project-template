959faa122ab77d18fa631ce808dbf431ae6353d0
Logan Bussell <github@loganbussell.com>
2026-02-21 20:31:07 -0800

    Clean out projects and set up for .NET 10

--- src/ClassLib/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿// SPDX-FileCopyrightText: Logan Bussell <https://github.com/lbussell>
-// SPDX-License-Identifier: MIT
-
-namespace MyCompany.MySolution.ClassLib;
-
-public static class Class1
-{
-    public const string Greeting = "Hello World!";
-}
